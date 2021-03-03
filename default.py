61e0766b5d9aef758af1eb8d71e4c8fbeb872781
Shaine Gordon <sgordon@realmdigital.co.za>
2021-03-03 22:24:19 +0200

    refactored to support Kodi Matrix

--- default.py
+++ default.py
@@ -18,6 +18,8 @@
 #
 
 import os
+import sys
+
 import xbmcaddon
 import xbmcgui
 import xbmc
@@ -28,15 +30,14 @@ addon_path = addon.getAddonInfo('path')
 
 __addon__ = xbmcaddon.Addon()
 __addonid__ = __addon__.getAddonInfo('id')
-__cwd__ = __addon__.getAddonInfo('path').decode("utf-8")
-__resource__ = xbmc.translatePath( os.path.join( __cwd__, 'resources', 'lib' ).encode("utf-8") ).decode("utf-8")
+__cwd__ = __addon__.getAddonInfo('path')
+__resource__ = xbmc.translatePath(os.path.join(__cwd__, 'resources', 'lib').encode("utf-8"))
 
 sys.path.append(__resource__)
 
-    
-
 if __name__ == '__main__':
     import gui
+
     screensaver_gui = gui.Screensaver(
         'unaryclock.xml',
         addon_path,
--- resources/lib/controller.py
+++ resources/lib/controller.py
@@ -23,38 +23,42 @@ import datetime
 
 import xbmc
 
+
 class Controller(threading.Thread):
-  
+
     def __init__(self, log_callback, drawClock_callback, showSeconds, redrawInterval):
-      super(Controller, self).__init__()
-      self.log_callback = log_callback
-      self.drawClock_callback = drawClock_callback
-      self.showSeconds = showSeconds
-      self.redrawInterval = redrawInterval
-      self.waitCondition = threading.Condition()
-      self._stop = False
-      
+        super(Controller, self).__init__()
+        self.log_callback = log_callback
+        self.drawClock_callback = drawClock_callback
+        self.showSeconds = showSeconds
+        self.redrawInterval = redrawInterval
+        self.waitCondition = threading.Condition()
+        self._stop = False
+
     def run(self):
-         self.waitCondition.acquire()
-         while not self.shouldStop():
-             self.now = datetime.datetime.today()
-             if (self.now.second % self.redrawInterval == 0):
+        self.waitCondition.acquire()
+        while not self.shouldStop():
+            self.log('while loop')
+            self.now = datetime.datetime.today()
+            if (self.now.second % self.redrawInterval == 0):
                 self.drawClock_callback(False)
-             elif (self.showSeconds):
+            elif (self.showSeconds):
                 self.drawClock_callback(True)
 
-             self.waitFor =  1000000 - self.now.microsecond
-             self.waitCondition.wait(float(self.waitFor) / 1000000)
-         self.waitCondition.release()
-      
-      
+            self.waitFor = 1000000 - self.now.microsecond
+            self.waitCondition.wait(float(self.waitFor) / 1000000)
+        self.waitCondition.release()
+
     def shouldStop(self):
-        if (xbmc.abortRequested):
+        if (xbmc.Monitor().abortRequested()):
             return True
         return self._stop
-        
+
     def stop(self):
         self.waitCondition.acquire()
         self._stop = True
         self.waitCondition.notifyAll()
         self.waitCondition.release()
+
+    def log(self, msg):
+        xbmc.log(u'Unary Clock Screensaver: %s' % msg)
